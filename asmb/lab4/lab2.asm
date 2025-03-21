e412d09da36da0d9847fb6d9335feab737a50943
Timur <timurtein@gmail.com>
2025-03-21 10:49:01 +0300

    working output + math

--- asmb/lab4/lab2.asm
+++ asmb/lab4/lab2.asm
@@ -1,64 +1,30 @@
-section .data 
-    prompt_a db "Enter: ", 0
+%include "./lib.asm" ; либа для преобразовани 
 
+section .data
+    arr dw 1, 1, 1, 1, 1, 1
+        dw 1, 1, 1, 1, 1, 1
+        dw 1, 1, 1, 1, 1, 1
+        dw 1, 1, 1, 1, 1, 1 ; пример массива 4x6
+    sums dw 0, 0, 0, 0, 0, 0 ; массив для сумм столбцов
     outArr db "Your array: ", 0
-    lenOut equ $-outArr
+    lenOut equ $ - outArr
     res db "Columns sum: ", 0
-    lenRes equ $-res
-
-section .bss
-    input_buffer resb 10 
-    output_buffer resb 10
-    arr resw 24 ; матрица (4 строки по 6 столбцов)
-    sums resw 6 ; массив для сумм столбцов (6 столбцов)
+    lenRes equ $ - res
+    space db " ", 0
+    output_buffer times 10 db 0
 
 section .text
-    global _start
-
-%include "./lib.asm" ; либа с преобразованиями
+global _start
 
 _start:
-    ; Ввод матрицы
-    mov ecx, 24 ; количество элементов матрицы
-    mov edi, arr ; указатель на начало массива
-
-input_loop:
-    push ecx ; сохраняем ecx
-
-    ; Вывод приглашения для ввода
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, prompt_a
-    mov edx, 7
-    int 0x80
-
-    ; Ввод числа
-    mov eax, 3
-    mov ebx, 0
-    mov ecx, input_buffer
-    mov edx, 16
-    int 0x80
-
-    ; Null-terminate input buffer
-    mov byte [input_buffer + eax - 1], 0
-
-    ; Преобразование ввода в число
-    mov esi, input_buffer ; используем esi для передачи адреса буфера
-    call StrToInt
-    mov [edi], eax ; сохраняем число в массиве
-    add edi, 2 ; переход к следующему элементу
-
-    pop ecx ; восстанавливаем ecx
-    loop input_loop
-
     ; Сброс сумм столбцов
     mov ecx, 6
     mov edi, sums
     xor eax, eax
 
 reset_column_sums:
-    mov [edi], eax
-    add edi, 4
+    mov [edi], ax ; обнуляем сумму столбца
+    add edi, 2    ; переходим к следующему столбцу
     loop reset_column_sums
 
     ; Суммирование элементов каждого столбца
@@ -67,7 +33,7 @@ reset_column_sums:
 
 sum_columns:
     xor eax, eax ; обнуляем сумму для текущего столбца
-    mov edx, 0 ; сброс счетчика строк
+    mov edx, 0   ; сброс счетчика строк
 
 sum_row:
     cmp edx, 4 ; проверяем, достигли ли конца строки
@@ -75,17 +41,18 @@ sum_row:
 
     ; Вычисляем адрес элемента
     mov ebx, edx ; копируем индекс строки
-    imul ebx, 6 ; умножаем на количество столбцов
+    imul ebx, 6  ; умножаем на количество столбцов
     add ebx, ecx ; добавляем индекс столбца (ecx)
-    mov eax, [arr + ebx * 2] ; загружаем элемент массива
+    dec ebx      ; корректируем индекс (так как ecx начинается с 1)
+    mov ax, [arr + ebx * 2] ; загружаем элемент массива
 
-    add [edi], eax ; добавляем к сумме столбца
+    add [edi], ax ; добавляем к сумме столбца
 
     inc edx ; переход к следующей строке
     jmp sum_row
 
 store_sum:
-    add edi, 4 ; переход к следующему столбцу
+    add edi, 2 ; переход к следующему столбцу
     loop sum_columns
 
     ; Вывод массива
@@ -101,18 +68,26 @@ store_sum:
 
 print_array:
     push ecx
-    mov eax, [esi]
+    mov ax, [esi] ; загружаем элемент массива (16-битное значение)
+    movzx eax, ax ; расширяем до 32 бит
     mov edi, output_buffer
-    call IntToStr
+    call IntToStr ; преобразуем число в строку
 
     ; Вывод элемента
     mov eax, 4
     mov ebx, 1
     mov ecx, output_buffer
-    mov edx, 10 ; Длина буфера вывода
+    mov edx, eax ; длина буфера вывода (результат IntToStr)
     int 0x80
 
-    add esi, 4
+    ; Вывод пробела между элементами
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, space
+    mov edx, 1
+    int 0x80
+
+    add esi, 2 ; переходим к следующему элементу массива
     pop ecx
     loop print_array
 
@@ -124,22 +99,30 @@ print_array:
     int 0x80
 
     mov ecx, 6 ; количество столбцов
-    mov edi, sums ; указатель на массив сумм
+    mov esi, sums ; указатель на массив сумм
 
 print_column_sums:
     push ecx
-    mov eax, [edi]
-    mov esi, output_buffer
-    call IntToStr
+    mov ax, [esi] ; загружаем сумму столбца (16-битное значение)
+    movzx eax, ax ; расширяем до 32 бит
+    mov edi, output_buffer
+    call IntToStr ; преобразуем число в строку
 
     ; Вывод суммы
     mov eax, 4
     mov ebx, 1
     mov ecx, output_buffer
-    mov edx, 10 ; Длина буфера вывода
+    mov edx, eax ; длина буфера вывода (результат IntToStr)
     int 0x80
 
-    add edi, 4
+    ; Вывод пробела между суммами
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, space
+    mov edx, 1
+    int 0x80
+
+    add esi, 2 ; переходим к следующему столбцу
     pop ecx
     loop print_column_sums
 
--- asmb/lab4/lib.asm
+++ asmb/lab4/lib.asm
@@ -1,77 +1,92 @@
-    section _text
+section .text
+
+; Преобразование строки в число
+; Вход:  esi - указатель на строку
+; Выход: eax - число, ebx - флаг ошибки (0 - успех, 1 - ошибка)
 StrToInt:
-         push   edi
-         mov    bh, '9'
-         mov    bl, '0'
-         push   esi     ; ��������� ����� �������� ������
-         cmp    byte[esi], '-'
-         jne   .prod
-         inc    esi     ; ���������� ����
-.prod    cld
-         xor    di, di  ; �������� ������� �����
-.cycle:  lodsb          ; ��������� ������ (�����)
-         cmp    al, 10  ; ���� 10, �� �� �����
-         je     .Return
-         cmp    al, bl  ; ���������� � ����� ����
-         jb     .Error  ; "����" � ������
-         cmp    al, bh  ; ���������� � ����� ������ 
-         ja     .Error  ; "����" � ������
-         sub    al, 30h ; �������� ����� �� �������
-         cbw            ; ��������� �� �����
-         push   ax      ; ��������� � �����
-         mov    ax, 10  ; ������� 10 � AX
-         mul    di      ; ��������, ��������� � DX:AX
-         pop    di      ; � DI � ��������� �����
-         add    ax, di
-         mov    di, ax  ; � DI � ����������� �����        
-         jmp    .cycle
-.Return: pop    esi
-         mov    ebx, 0
-         cmp    byte[esi], '-'
-         jne    .J
-         neg    di
-.J       mov    ax, di
-         cwde
-         jmp    .R
-.Error:  pop    esi
-         mov    eax, 0
-         mov    ebx, 1
-.R       pop    edi
-         ret
-IntToStr: 
-         push   edi
-         push   ebx
-         push   edx
-         push   ecx
-		 push   esi
-		 mov    byte[esi],0 ; �� ����� �����
-         cmp    eax,0
-         jge    .l1
-         neg    eax
-         mov    byte[esi],'-'
-.l1      mov    byte[esi+6],10
-         mov    edi,5
-         mov    bx,10
-.again:  cwd           ; ��������� ����� �� ��������
-         div    bx     ; ����� ��������� �� 10
-         add    dl,30h ; �������� �� ������� ��� �����
-         mov    [esi+edi],dl ; ����� ������ � ������
-         dec    edi    ; ��������� ��������� ��  
-                       ; ���������� �������
-         cmp    ax, 0  ; ������������� ��� �����?
-         jne    .again
-         mov    ecx, 6
-         sub    ecx, edi ; ����� ����������+����
-		 mov    eax,ecx
-		 inc    eax      ; ����� ����������+0�
-         inc    esi      ; ���������� ����
-		 push   esi
-         lea    esi,[esi+edi] ; ������ ����������
-		 pop    edi
-         rep movsb
-         pop    esi  
-         pop    ecx
-         pop    edx
-         pop    ebx
-         pop    edi
-         ret
+    push   edi
+    push   esi
+    mov    bh, '9'
+    mov    bl, '0'
+    cmp    byte [esi], '-'
+    jne    .prod
+    inc    esi     ; Пропуск знака минус
+.prod:
+    cld
+    xor    edi, edi  ; Обнуляем результат
+.cycle:
+    lodsb           ; Загружаем символ (байт)
+    cmp    al, 10   ; Если 10, то конец строки
+    je     .Return
+    cmp    al, bl   ; Сравниваем с цифрой '0'
+    jb     .Error   ; Если меньше, то ошибка
+    cmp    al, bh   ; Сравниваем с цифрой '9'
+    ja     .Error   ; Если больше, то ошибка
+    sub    al, 30h  ; Преобразуем символ в цифру
+    cbw             ; Расширяем до слова
+    push   eax      ; Сохраняем цифру в стеке
+    mov    eax, edi ; Загружаем текущий результат
+    mov    ecx, 10
+    mul    ecx      ; Умножаем на 10
+    pop    ecx      ; Восстанавливаем цифру
+    add    eax, ecx ; Добавляем к результату
+    mov    edi, eax ; Сохраняем результат
+    jmp    .cycle
+.Return:
+    pop    esi
+    mov    ebx, 0
+    cmp    byte [esi], '-'
+    jne    .J
+    neg    edi
+.J:
+    mov    eax, edi
+    jmp    .R
+.Error:
+    pop    esi
+    mov    eax, 0
+    mov    ebx, 1
+.R:
+    pop    edi
+    ret
+
+; Преобразование числа в строку
+; Вход:  eax - число, edi - указатель на буфер вывода
+; Выход: eax - длина строки
+IntToStr:
+    push   edi
+    push   ebx
+    push   edx
+    push   ecx
+    push   esi
+    mov    byte [edi], 0 ; Очистка буфера
+    cmp    eax, 0
+    jge    .l1
+    neg    eax
+    mov    byte [edi], '-'
+.l1:
+    mov    byte [edi+6], 10 ; Добавляем символ новой строки
+    mov    esi, 5           ; Индекс для записи символов
+    mov    bx, 10
+.again:
+    cwd           ; Расширяем знак для деления
+    div    bx     ; Делим на 10
+    add    dl, 30h ; Преобразуем остаток в символ
+    mov    [edi+esi], dl ; Записываем символ в буфер
+    dec    esi    ; Уменьшаем индекс
+    cmp    ax, 0  ; Проверяем, закончилось ли число
+    jne    .again
+    mov    ecx, 6
+    sub    ecx, esi ; Вычисляем длину строки
+    mov    eax, ecx
+    inc    eax      ; Учитываем символ новой строки
+    inc    edi      ; Пропускаем знак минус
+    push   edi
+    lea    esi, [edi+esi] ; Указатель на начало строки
+    pop    edi
+    rep movsb       ; Копируем строку в буфер
+    pop    esi
+    pop    ecx
+    pop    edx
+    pop    ebx
+    pop    edi
+    ret
