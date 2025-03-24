392b4427a5c787c48ebe248a7188a9111cf4f1c7
Timur <timurtein@gmail.com>
2025-03-24 16:07:19 +0300

    code for lab4 is done

--- asmb/lab4/lab2.asm
+++ asmb/lab4/lab2.asm
@@ -1,59 +1,57 @@
-%include "./lib.asm" ; либа для преобразовани 
+%include "./lib.asm" ; либа с функциями StrToInt и IntToStr
 
 section .data
-    arr dw 1, 1, 1, 1, 1, 1
-        dw 1, 1, 1, 1, 1, 1
-        dw 1, 1, 1, 1, 1, 1
-        dw 1, 1, 1, 1, 1, 1 ; пример массива 4x6
-    sums dw 0, 0, 0, 0, 0, 0 ; массив для сумм столбцов
-    outArr db "Your array: ", 0
+    sums dw 0,0,0,0,0,0 ; массив сумм
+    outArr db "Your array:", 10
     lenOut equ $ - outArr
-    res db "Columns sum: ", 0
+    res db 10, "Columns sums:", 10
     lenRes equ $ - res
     space db " ", 0
-    output_buffer times 10 db 0
+    newline db 10, 0
+    num_prompt db "Enter number: ", 0
+    len_num_prompt equ $ - num_prompt
+    input_buffer times 16 db 0   ; буфер для ввода
+    output_buffer times 10 db 0  ; буфер для вывода
+
+section .bss
+    arr resw 1 ; массив 2х6
 
 section .text
 global _start
 
 _start:
-    ; Сброс сумм столбцов
-    mov ecx, 6
-    mov edi, sums
-    xor eax, eax
+    ; Ввод 24 чисел
+    mov ecx, 24 ; регистр счётчик
+    mov edi, arr
 
-reset_column_sums:
-    mov [edi], ax ; обнуляем сумму столбца
-    add edi, 2    ; переходим к следующему столбцу
-    loop reset_column_sums
+input_loop:
+    push ecx
+    push edi
 
-    ; Суммирование элементов каждого столбца
-    mov ecx, 6 ; количество столбцов
-    mov edi, sums ; указатель на массив сумм
+    ; Вывод приглашения
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, num_prompt
+    mov edx, len_num_prompt
+    int 0x80
 
-sum_columns:
-    xor eax, eax ; обнуляем сумму для текущего столбца
-    mov edx, 0   ; сброс счетчика строк
+    ; Чтение ввода
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, input_buffer
+    mov edx, 16
+    int 0x80
 
-sum_row:
-    cmp edx, 4 ; проверяем, достигли ли конца строки
-    jge store_sum
+    ; Преобразование в число
+    mov esi, input_buffer
+    call StrToInt
 
-    ; Вычисляем адрес элемента
-    mov ebx, edx ; копируем индекс строки
-    imul ebx, 6  ; умножаем на количество столбцов
-    add ebx, ecx ; добавляем индекс столбца (ecx)
-    dec ebx      ; корректируем индекс (так как ecx начинается с 1)
-    mov ax, [arr + ebx * 2] ; загружаем элемент массива
-
-    add [edi], ax ; добавляем к сумме столбца
-
-    inc edx ; переход к следующей строке
-    jmp sum_row
-
-store_sum:
-    add edi, 2 ; переход к следующему столбцу
-    loop sum_columns
+    ; Сохранение числа
+    pop edi
+    mov [edi], ax
+    add edi, 2
+    pop ecx
+    loop input_loop
 
     ; Вывод массива
     mov eax, 4
@@ -62,34 +60,74 @@ store_sum:
     mov edx, lenOut
     int 0x80
 
-    ; Вывод элементов массива
-    mov ecx, 24 ; количество элементов в массиве
-    mov esi, arr ; указатель на начало массива
+    ; Вывод 4 строк по 6 элементов
+    mov ecx, 4
+    mov esi, arr
 
-print_array:
+print_rows:
     push ecx
-    mov ax, [esi] ; загружаем элемент массива (16-битное значение)
-    movzx eax, ax ; расширяем до 32 бит
-    mov edi, output_buffer
-    call IntToStr ; преобразуем число в строку
+    mov ecx, 6
 
-    ; Вывод элемента
+print_cols:
+    push ecx
+    mov ax, [esi]
+    movzx eax, ax
+    mov edi, output_buffer
+    call IntToStr
+
+    ; Вывод числа
+    push eax        ; сохраняем длину строки
     mov eax, 4
     mov ebx, 1
     mov ecx, output_buffer
-    mov edx, eax ; длина буфера вывода (результат IntToStr)
+    pop edx         ; возвращаем длину из стека
     int 0x80
 
-    ; Вывод пробела между элементами
+    ; Пробел между числами (кроме последнего)
+    pop ecx
+    cmp ecx, 1
+    je .no_space
+    push ecx
     mov eax, 4
     mov ebx, 1
     mov ecx, space
     mov edx, 1
     int 0x80
-
-    add esi, 2 ; переходим к следующему элементу массива
     pop ecx
-    loop print_array
+.no_space:
+    add esi, 2
+    loop print_cols
+
+    ; Перенос строки
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, newline
+    mov edx, 1
+    int 0x80
+
+    pop ecx
+    loop print_rows
+
+    ; Расчет сумм столбцов
+    mov ecx, 6
+    mov edi, sums
+    mov ebx, 0
+
+calc_sums:
+    xor eax, eax
+    mov dx, [arr + ebx]
+    add ax, dx
+    mov dx, [arr + ebx + 12]
+    add ax, dx
+    mov dx, [arr + ebx + 24]
+    add ax, dx
+    mov dx, [arr + ebx + 36]
+    add ax, dx
+    
+    mov [edi], ax
+    add edi, 2
+    add ebx, 2
+    loop calc_sums
 
     ; Вывод сумм столбцов
     mov eax, 4
@@ -98,35 +136,40 @@ print_array:
     mov edx, lenRes
     int 0x80
 
-    mov ecx, 6 ; количество столбцов
-    mov esi, sums ; указатель на массив сумм
+    mov ecx, 6
+    mov esi, sums
 
-print_column_sums:
+print_sums:
     push ecx
-    mov ax, [esi] ; загружаем сумму столбца (16-битное значение)
-    movzx eax, ax ; расширяем до 32 бит
+    mov ax, [esi]
+    movzx eax, ax
     mov edi, output_buffer
-    call IntToStr ; преобразуем число в строку
+    call IntToStr
 
     ; Вывод суммы
+    push eax
     mov eax, 4
     mov ebx, 1
     mov ecx, output_buffer
-    mov edx, eax ; длина буфера вывода (результат IntToStr)
+    pop edx
     int 0x80
 
-    ; Вывод пробела между суммами
+    ; Пробел между суммами (кроме последней)
+    pop ecx
+    cmp ecx, 1
+    je .no_space_sum
+    push ecx
     mov eax, 4
     mov ebx, 1
     mov ecx, space
     mov edx, 1
     int 0x80
-
-    add esi, 2 ; переходим к следующему столбцу
     pop ecx
-    loop print_column_sums
+.no_space_sum:
+    add esi, 2
+    loop print_sums
 
     ; Завершение программы
-    mov eax, 1 ; sys_exit
+    mov eax, 1
     xor ebx, ebx
     int 0x80
